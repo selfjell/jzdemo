b3d8697520b16633a4e49018ff70d7c99e9e6165
Håvard Trondsen <haavard.trondsen@sb1ostlandet.no>
2026-08-12 15:08:35 +0200

    Add visual examples for HTMX and Alpine demos

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -6,6 +6,7 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.html.*
 import io.ktor.server.http.content.staticResources
+import io.ktor.server.request.receiveParameters
 import kotlinx.html.*
 import kotlinx.html.stream.createHTML
 import no.eksempel.html.index
@@ -29,6 +30,24 @@ fun Application.configureRouting() {
         get("/resource") {
             val html = createHTML().button {
                 renderResourceFetchingButton("#replaceable")
+            }
+            call.respondText(html, ContentType.Text.Html)
+        }
+
+        get("/example") {
+            val html = createHTML().div {
+                p { +"Hello"}
+            }
+            call.respondText(html, ContentType.Text.Html)
+        }
+
+        put("/example") {
+            val params = call.receiveParameters()
+            val att1 = params["att1"] ?: ""
+            val att2 = params["att2"] ?: ""
+            val html = createHTML().div {
+                p { +att1 }
+                p { +att2 }
             }
             call.respondText(html, ContentType.Text.Html)
         }
